22a49c3f4b28665774c8a4f8dcb0947df53499e7
Zac <zac.minidude140@Gmail.com>
2023-09-07 08:34:06 -0600

    Added ByVal and ByRef examples

--- MethodCallsEample/MethodCallsExample.vb
+++ MethodCallsEample/MethodCallsExample.vb
@@ -12,8 +12,15 @@ Module MethodCallsExample
 
     'Sub main always runs.  It will go to the routine called and return to main after sub routine finishes
     Sub Main()
+        Dim userMessage As String = "Hello"
+
         SayHello()
         SumOf(5, 7)
+        ProductOf(4, 5)
+
+        Console.WriteLine($"In Main: {userMessage}")
+        SendMessage(userMessage)
+        Console.WriteLine($"In Main: {userMessage}")
         Console.ReadLine()
     End Sub
 
@@ -25,6 +32,21 @@ Module MethodCallsExample
     'example with parameters or arguments
     Sub SumOf(firstNumber As Integer, secondNumber As Integer)
         Console.WriteLine(firstNumber + secondNumber)
+    End Sub
+
+    'byval saves copy of variable value and releases from memory when done 
+    '(above example shows byval Is default in method calls)
+    Sub ProductOf(ByVal firstNumber As Integer, ByVal SecondNumber As Integer)
+        Console.WriteLine(firstNumber * SecondNumber)
+    End Sub
+
+    'by reference doesn't copy input value it can rewrite the original value
+    'following example rewrites userMessage value outside of sub routine
+    Sub SendMessage(ByRef message As String)
+        Console.WriteLine($"In SendMessage: {message}.")
+        message = "Goodbye."
+        Console.WriteLine($"In SendMessage: {message}")
+
     End Sub
 
 End Module
