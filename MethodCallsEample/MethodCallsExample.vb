92d179f0ecd6cc6b1625aa4e9be942e0e5bfbb31
Zac <zac.minidude140@Gmail.com>
2023-09-07 08:12:34 -0600

    Add Basic SayHello sub routine.

--- MethodCallsEample/MethodCallsExample.vb
+++ MethodCallsEample/MethodCallsExample.vb
@@ -2,7 +2,7 @@
 'RCET 2265
 'Fall 2023
 'Method Call Example
-'https://github.com/Minidude140
+'https://github.com/Minidude140/MethodCallsEample.git
 
 
 Option Explicit On
@@ -11,7 +11,12 @@ Option Strict On
 Module MethodCallsExample
 
     Sub Main()
+        SayHello()
+        Console.ReadLine()
+    End Sub
 
+    Sub SayHello()
+        Console.WriteLine("Hello")
     End Sub
 
 End Module
