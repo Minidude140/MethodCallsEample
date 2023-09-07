04b9211a0c701c5f6458bcfaddf77cc5db8cd40d
Zac <zac.minidude140@Gmail.com>
2023-09-07 08:21:14 -0600

    Added sub with parameters

--- MethodCallsEample/MethodCallsExample.vb
+++ MethodCallsEample/MethodCallsExample.vb
@@ -10,13 +10,21 @@ Option Strict On
 
 Module MethodCallsExample
 
+    'Sub main always runs.  It will go to the routine called and return to main after sub routine finishes
     Sub Main()
         SayHello()
+        SumOf(5, 7)
         Console.ReadLine()
     End Sub
 
+    'Basic sub routine says helllo
     Sub SayHello()
         Console.WriteLine("Hello")
+    End Sub
+
+    'example with parameters or arguments
+    Sub SumOf(firstNumber As Integer, secondNumber As Integer)
+        Console.WriteLine(firstNumber + secondNumber)
     End Sub
 
 End Module
